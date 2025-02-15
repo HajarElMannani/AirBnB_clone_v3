a2991e0b9528260be25c161ad8b704ca4f5a91dd
HajarElMannani <laso125@hotmail.com>
2025-02-15 13:02:44 -0800

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,10 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv, environ
-from flask_cors import CORS
+from os import getenv
+
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origin": "0.0.0.0"}})
 app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
 app.register_blueprint(app_views)
 
@@ -19,11 +18,5 @@ def teardown(exeption):
 
 
 if __name__ == "__main__":
-    h_host = '0.0.0.0'
-    h_port = 5000
-    if environ.get('HBNB_API_HOST'):
-        h_host = getenv('HBNB_API_HOST')
-    if environ.get('HBNB_API_PORT'):
-        h_port = getenv('HBNB_API_PORT')
-        
-    app.run(host=h_host, port=h_port, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
