195c010db1649246e9e956f0f2509cba5b93d283
HajarElMannani <laso125@hotmail.com>
2025-02-16 13:45:28 -0800

    temporary

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -19,7 +19,7 @@ def users_get():
 @app_views.route('/users/<string:user_id>', methods=['GET'],
                  strict_slashes=False)
 def users_id(user_id):
-    '''retrieve user by id'''
+    retrieve user by id
     users = storage.get(User, user_id)
     if not users:
         abort(404)
@@ -29,7 +29,7 @@ def users_id(user_id):
 @app_views.route('/users/<string:user_id>', methods=['DELETE'],
                  strict_slashes=False)
 def user_delete(user_id=None):
-    '''delete a user by id'''
+    delete a user by id
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
@@ -40,7 +40,7 @@ def user_delete(user_id=None):
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def user_post():
-    '''Create an user'''
+    Create an user
     user = request.get_json(silent=True)
     if user is None:
         abort(400, "Not a JSON")
@@ -57,7 +57,7 @@ def user_post():
 @app_views.route('/users/<string:user_id>', methods=['PUT'],
                  strict_slashes=False)
 def user_update(user_id):
-    '''update user'''
+   update user
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
