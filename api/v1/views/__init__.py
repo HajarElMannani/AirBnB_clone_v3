3ef498f042bb8112865b70fdf36840198e1cb187
HajarElMannani <laso125@hotmail.com>
2025-02-17 08:26:58 -0800

    add documentation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""
+view/__int__ file
+"""
+
+
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import * #c
-from api.v1.views.states import * #c
-from api.v1.views.cities import * #c
-from api.v1.views.amenities import * #c
-from api.v1.views.users import * #c
-from api.v1.views.places import * #c
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
